5d7de351748e7367b0152d2124487f8cb77cfd27
haicomykia <undefinedsynthesis@gmail.com>
2023-11-19 01:56:34 +0900

    $_FILEを追加

--- html/index.js
+++ html/index.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
  * @see https://stackoverflow.com/questions/12989442/uploading-multiple-files-using-formdata
  */
 $("#files").on('input', function(e){
-  console.log(this.files);
   const fd = new FormData();
   for (let i = 0; i < this.files.length; i++) {
     fd.append('files', this.files[i]);
--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +7,13 @@
   <title>Document</title>
 </head>
 <body>
-  <input type="file" name="files" id="files" multiple accept="image/*, .pdf">
+  <form action="" method="post" enctype="multipart/form-data" action="index.php">
+    <input type="file" name="files[]" id="files" multiple accept="image/*, .pdf">
+    <input type="submit" value="Send">
+  </form>
+  <?php
+    var_dump($_FILES['files']['name']);
+  ?>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="./index.js"></script>
 </body>
