9ed92c3f7b66e4285508ff6c40d12c7a36f0dcf8
haicomykia <undefinedsynthesis@gmail.com>
2023-11-19 01:31:35 +0900

    参考ソースを追加

--- html/index.js
+++ html/index.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   
 });
 
+/**
+ * @see https://stackoverflow.com/questions/12989442/uploading-multiple-files-using-formdata
+ */
 $("#files").on('input', function(e){
   console.log(this.files);
   const fd = new FormData();
